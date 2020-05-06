e9a367d6b4d489840657c9ce12a556e94bce76cd
Alejandro Harrison <harri171@csu.fullerton.edu>
2020-05-06 11:51:44 -0700

    Still getting errors

--- ices_algs.hpp
+++ ices_algs.hpp
@@ -39,16 +39,25 @@ unsigned int iceberg_avoiding_exhaustive(const grid& setting) {
   unsigned int count_paths = 0;
   // TODO: implement the exhaustive optimization algorithm, then delete this
   // comment.
-  int bit;
+
 
   for (int i =0; i< (pow(2,steps)-1); i++){
 	path exhaust(setting);
-  for (int k = 0; k<steps-1; k++){
-	bit = ((i>>k)/2) & 1;
-  if (bit == 1) exhaust.add_step(STEP_DIRECTION_RIGHT);
-  else exhaust.add_step(STEP_DIRECTION_DOWN);
-  }
-  count_paths++;
+      for (int k = 0; k<steps; k++){
+	    int bit;
+	    bit = (i>>k) & 1;
+         if (bit == 1){
+	   if(exhaust.is_step_valid(STEP_DIRECTION_RIGHT))
+	       exhaust.add_step(STEP_DIRECTION_RIGHT);
+         }
+
+	else {
+	  if (exhaust.is_step_valid(STEP_DIRECTION_DOWN))
+	       exhaust.add_step(STEP_DIRECTION_DOWN);
+        }
+      }
+  if (exhaust.final_row() == setting.rows()-1 && exhaust.final_column() == setting.columns()-1)
+ 	count_paths++;
 }
 
   return count_paths;
@@ -85,7 +94,7 @@ unsigned int iceberg_avoiding_dyn_prog(const grid& setting) {
 	  if( j >0 && A[i][j-1] != 0)
 		from_left = A[i][j-1];
 
-	if(from_above = 0  && from_left = 0)
+	if(from_above == 0  && from_left == 0)
 	   {
 		A[i][j] = 0;
 	   }
